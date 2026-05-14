05298713d4050435dd2987ef272f7141d15bab33
nalinbeligaswatta <your-email@example.com>
2026-05-14 08:52:28 +0530

    fix minSdk to 26

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.plant_disease_detector"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
